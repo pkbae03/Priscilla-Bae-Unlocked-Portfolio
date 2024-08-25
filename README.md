eae014e3afcfaa9589273505e1cecc736e8e8604
pkbae03 <143964064+pkbae03@users.noreply.github.com>
2024-08-25 19:30:08 -0400

    adding all info

---
+++ README.md
@@ -0,0 +1,6 @@
+Website Link: https://priscillabae.github.io/Priscilla-Bae-s-Portfolio/index.html
+
+
+
+
+Respository Link: https://github.com/priscillabae/Priscilla-Bae-s-Portfolio.git
---
+++ script.js
@@ -0,0 +1,61 @@
+const type1_str = "Hi, my name's Priscilla, & welcome to my website ♥";
+
+window.addEventListener("load", function() {
+  executeAsynchronously([
+    [type1_str, "type1", 100],
+  ]);
+});
+
+function executeAsynchronously(texts) {
+  texts.forEach(args => type(...args));
+}
+
+function type(text, id, delay) {
+  const elem = document.getElementById(id);
+  for (let i = 0; i < text.length; i++) {
+    setTimeout(() => {
+      elem.textContent += text.charAt(i);
+    }, i * delay);
+  }
+}
+
+
+let items = document.querySelectorAll('.slider .item');
+    let next = document.getElementById('next');
+    let prev = document.getElementById('prev');
+    
+    let active = 3;
+    function loadShow(){
+        let stt = 0;
+        items[active].style.transform = `none`;
+        items[active].style.zIndex = 1;
+        items[active].style.filter = 'none';
+        items[active].style.opacity = 1;
+        for(var i = active + 1; i < items.length; i++){
+            stt++;
+            items[i].style.transform = `translateX(${120*stt}px) scale(${1 - 0.2*stt}) perspective(16px) rotateY(-1deg)`;
+            items[i].style.zIndex = -stt;
+            items[i].style.filter = 'blur(5px)';
+            items[i].style.opacity = stt > 2 ? 0 : 0.6;
+        }
+        stt = 0;
+        for(var i = active - 1; i >= 0; i--){
+            stt++;
+            items[i].style.transform = `translateX(${-120*stt}px) scale(${1 - 0.2*stt}) perspective(16px) rotateY(1deg)`;
+            items[i].style.zIndex = -stt;
+            items[i].style.filter = 'blur(5px)';
+            items[i].style.opacity = stt > 2 ? 0 : 0.6;
+        }
+    }
+    loadShow();
+    next.onclick = function(){
+        active = active + 1 < items.length ? active + 1 : active;
+        loadShow();
+    }
+    prev.onclick = function(){
+        active = active - 1 >= 0 ? active - 1 : active;
+        loadShow();
+    }
+
+
+
